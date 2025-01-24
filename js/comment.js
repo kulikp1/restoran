32ee9ada2551576f079594a2d84ec7b91d12c314
kulikp1 <pashakylik2@gmail.com>
2025-01-24 23:34:52 +0200

    translate testimonials text

--- js/comment.js
+++ js/comment.js
@@ -4,24 +4,25 @@ const userName = document.querySelector(".comment-name");
 
 const testimonials = [
   {
-    name: 'Микола',
+    name: 'Mykola',
     photoUrl:
       'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
-    text: "Я надовго запам'ятаю мій День народження, проведений у цьому ресторані! Шматок рідної Вірменії! Окреме дякую за комплімент у вигляді фруктової тарілки. Будемо рекомендувати цей ресторан своїм друзям та родичам також за кордоном, подорожуючих до Чернігова!!!",
+    text: "I will long remember my birthday celebrated at this restaurant! A piece of Armenia right here! A special thanks for the complimentary fruit plate. We will recommend this restaurant to our friends and relatives abroad traveling to Chernihiv!!!",
   },
   {
-    name: 'Петро',
+    name: 'Petro',
     photoUrl:
       'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
-    text: 'Щойно занурившись у атмосферу цього ресторану, я відчув, що це справжнє відкриття! Повний розквіт смаку в кожній страві, дивовижна гармонія ароматів та ідеально підібрані інгредієнти. Але не лише смакові враження вражають - персонал тут дійсно знає, як зробити кожен момент особливим. З нетерпінням чекаю на наступний візит!',
+    text: "From the moment I immersed myself in the atmosphere of this restaurant, I felt it was a true discovery! A complete bloom of flavor in every dish, incredible harmony of aromas, and perfectly chosen ingredients. But it's not only about the taste sensations – the staff truly knows how to make every moment special. I can't wait for my next visit!",
   },
-{
-    name: 'Василь',
+  {
+    name: 'Vasyl',
     photoUrl:
       'https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
-    text: "З кожним ковтком і кожним переповненим смаком келиха я переконуюся: цей ресторан - справжнє кулінарне відкриття! Тут їжа не просто їжа - це відчуття, що кожен кулінарний твір твориться з любов'ю та професійним майстерством. А атмосфера в захоплюючому ритмі розмов та сміху доповнює естетичне задоволення від кожного страви. Обов'язково повернуся ще й не один раз, щоб насолодитися цим кулінарним шедевром знову та знову!",
+    text: "With every sip and every taste-filled bite, I am convinced: this restaurant is a true culinary discovery! Here, food is not just food – it feels like every culinary creation is crafted with love and professional mastery. The atmosphere, with its captivating rhythm of conversations and laughter, enhances the aesthetic pleasure of each dish. I will definitely return many times to enjoy this culinary masterpiece again and again!",
   },
 ];
+
 
 let currentIndex = 0;
 
